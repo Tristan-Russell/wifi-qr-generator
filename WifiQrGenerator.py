00a2ab2a952019ff0d20304f2f0f49ddf2ad72d4
Tristan Russell <89198508+Tristan-Russell@users.noreply.github.com>
2023-12-05 19:00:59 +0200

    Update WifiQrGenerator.py

--- WifiQrGenerator.py
+++ WifiQrGenerator.py
@@ -1,6 +1,13 @@
 import qrcode
 
-def generate_wifi_qr_code(ssid, password):
+def generate_wifi_qr_code(ssid, password, filename):
+    """
+    Generate a WiFi QR code with the provided SSID and password.
+
+    :param ssid: SSID (Network Name)
+    :param password: WiFi Password
+    :param output_file: Output file name for the QR code image
+    """
     wifi_info = f'WIFI:T:WPA;S:{ssid};P:{password};;'
     qr = qrcode.QRCode(
         version=1,
@@ -12,9 +19,22 @@ def generate_wifi_qr_code(ssid, password):
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save("wifi_qr_code.png")
+    img.save(filename+'.png')
 
-if __name__ == "__main__":
+def get_wifi_credentials():
+    """
+    Get WiFi credentials (SSID and Password) from the user.
+    :return: Tuple containing SSID and Password
+    """
     ssid = input("Enter SSID (Network Name): ")
     password = input("Enter Password: ")
-    generate_wifi_qr_code(ssid, password)
+    filename = input("Enter filename (New Image Name): ")
+    return ssid, password, filename
+
+if __name__ == "__main__":
+    try:
+        ssid, password, filename = get_wifi_credentials()
+        generate_wifi_qr_code(ssid, password, filename)
+        print("WiFi QR code generated successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
